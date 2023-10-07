1b81afeae6b8663922d2141685939b3079bc4b87
asv-spb <asvdevpro@gmail.com>
2023-10-07 16:42:12 +0300

    add correct download procedure

--- install_selenium.sh
+++ install_selenium.sh
@@ -2,17 +2,40 @@ sudo apt update
 sudo apt install -y python3 python3-pip wget unzip
 pip3 install selenium python-dotenv
 
-# install Chrome && Chrome WebDriver
+# install current stable Chrome 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
-chrome_version=$(google-chrome --version | awk '{print $3}' | cut -d '.' -f 1)
-chrome_driver_url="https://chromedriver.storage.googleapis.com/$chrome_version/chromedriver_linux64.zip"
-wget "$chrome_driver_url" -O chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-sudo mv chromedriver /usr/local/bin/chromedriver
-sudo chmod +x /usr/local/bin/chromedriver
 
-# run selenium script
-chmod +x selenium_script.sh
-python3 selenium_script.py
-tail selenium_logs.txt
+# find current stable webdriver link
+installed_chrome_version=$(google-chrome --version | awk '{print $3}' | cut -d '.' -f 1)
+url="https://googlechromelabs.github.io/chrome-for-testing/"
+webdriver_version=$(curl -s "$url" | grep -Po 'https:\/\/edgedl\.me\.gvt1\.com\/edgedl\/chrome\/chrome-for-testing\/\K\d+\.\d+\.\d+\.\d+' | head -n 1)
+
+webdriver_version_prefix=$(echo "$webdriver_version" | cut -d '.' -f 1)
+webdriver_driver_url="https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$webdriver_version/linux64/chrome-linux64.zip"
+
+
+if [ "$installed_chrome_version" == "$webdriver_version_prefix" ]; then
+  echo "Версии совпадают, скачиваем webdriver:"
+  echo "-------------------------------------"
+
+  webdriver_driver_url="https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$webdriver_version/linux64/chrome-linux64.zip"
+  
+  wget "$webdriver_driver_url" -O chromedriver_linux64.zip
+  unzip chromedriver_linux64.zip
+  sudo mv chromedriver /usr/local/bin/chromedriver 
+  sudo chmod +x /usr/local/bin/chromedriver
+
+  chmod +x ./selenium_script.sh
+  python3 ./selenium_script.py
+  tail ./selenium_logs.txt
+
+else
+  echo "-------------------------------------"
+  echo "Версия установленного хром - $installed_chrome_version"
+  echo "Версия webdriver - $webdriver_version_prefix"
+  echo "Не совпадают, пропускаем установку!"
+  echo "-------------------------------------"
+fi
+
+
--- selenium_script.py
+++ selenium_script.py
@@ -1,9 +1,9 @@
-import logging
 import os
+import time
+import logging
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
 
 # Настройка логирования
 logging.basicConfig(
