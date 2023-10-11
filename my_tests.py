bc9bd491d3a18efa7bc4b8be463e5ea620a0bea2
asv-spb <asvdevpro@gmail.com>
2023-10-11 22:24:37 +0300

    add import

--- my_tests.py
+++ my_tests.py
@@ -1,8 +1,8 @@
-import os
-import time
-import logging
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from dotenv import load_dotenv
 load_dotenv()
+
+options = Options()
+options.headless = True
+driver = webdriver.Chrome(options=options)
