f407f55b98116382eeab5746d30f42a76ddb0e6b
asv-spb <asvdevpro@gmail.com>
2023-10-11 21:44:46 +0300

    add test.py

---
+++ my_tests.py
@@ -0,0 +1,8 @@
+import os
+import time
+import logging
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from dotenv import load_dotenv
+load_dotenv()
