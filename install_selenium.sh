14657dedc3c3caeae7a692fec4b410ad2bfb120b
asv-spb <asvdevpro@gmail.com>
2023-10-14 09:45:30 +0300

    gca

--- install_selenium.sh
+++ install_selenium.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install -y python3-tk python3-pip pythonpy python3.10 wget unzip
 pip3 install selenium python-dotenv
 
-# install current stable Chrome 
+# install current stable Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
@@ -23,10 +23,10 @@ if [ "$installed_chrome_version" == "$webdriver_version_prefix" ]; then
   echo "-------------------------------------"
 
   webdriver_driver_url="https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$webdriver_version/linux64/chrome-linux64.zip"
-  
+
   wget "$webdriver_driver_url" -O chromedriver_linux64.zip
   unzip chromedriver_linux64.zip
-  sudo mv chromedriver /usr/local/bin/chromedriver 
+  sudo mv chromedriver /usr/local/bin/chromedriver
   sudo chmod +x /usr/local/bin/chromedriver
 
   chmod +x ./selenium_script.sh
